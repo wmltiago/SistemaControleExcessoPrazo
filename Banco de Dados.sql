6ffd3e7d3e4b14ef6effad9f28700da01c625334
Luiz Silva <luizs.m@hotmail.com>
2017-02-09 19:45:20 -0300

    Luiz
    Adicionada a tabela de usuário e a permissão, lembrar de adicionar a foreign key na tabela usuário

--- Banco de Dados.sql
+++ Banco de Dados.sql
@@ -1,8 +1,30 @@
 CREATE TABLE presidio (
-	id_presidio int(10) NOT NULL AUTO_INCREMENT,
-	nome varchar(120) NOT NULL,
-	estado varchar(120) NOT NULL,
-  cidade varchar(120) NOT NULL,
-	tipo varchar(120) NOT NULL,
+	id_presidio int(1) NOT NULL AUTO_INCREMENT,
+	nome varchar(50) NOT NULL,
+	estado varchar(40) NOT NULL,
+        cidade varchar(50) NOT NULL,
+	tipo varchar(50) NOT NULL,
 	PRIMARY KEY (id_presidio)
 );
+
+
+
+CREATE TABLE usuario (
+	id_usuario int not null auto_increment,
+	nome varchar not null,
+	cpf int(15) not null unique,
+	senha varchar(15) not null,
+	endereco varchar(120) not null,
+	id_tipousuario int(1) not null,
+	PRIMARY KEY (id_usuario)
+);
+
+
+
+create table tipousuario (
+ id_tipousuario int(1) not null,
+ descricao varchar(15) not null,
+ primary key (id_tipousuario)
+);
+
+ALTER TABLE usuario ADD constraint usuario_fk0 foreign key (id_tipousuario) references tipousuario(id_tipousuario);
