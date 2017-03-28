bc2b8adc4b672d9b783ca91dc0f4310c0847a97b
wmltiago <tiagobhs@outlook.com>
2017-03-28 10:34:09 -0300

    Tiago
    Pesquisar presidio com AJAX finalizado (config do controller)

--- src/controller/PresidioController.java
+++ src/controller/PresidioController.java
@@ -21,21 +21,19 @@ public class PresidioController {
 	@RequestMapping("/exibirCadastrarPresidio")
 	public String exibirIncluirPresidio() {
 
-
-
-
 		return "presidio/CadastroPresidio";
 	}
 
-	//inclusao do @Valid, BindingResult result e do if para retornar ao exibirCadastrarPresidio//
+	// inclusao do @Valid, BindingResult result e do if para retornar ao
+	// exibirCadastrarPresidio//
 	@RequestMapping("/cadastrarPresidio")
-	public String incluirPresidio(@Valid Presidio presidio, BindingResult result,Model model) {
+	public String incluirPresidio(@Valid Presidio presidio, BindingResult result, Model model) {
 
 		if (result.hasErrors()) {
 			return "forward:exibirCadastrarPresidio";
-			}
-		
-		model.addAttribute("msg", "Cadastrado Com Sucesso!" );
+		}
+
+		model.addAttribute("msg", "Cadastrado Com Sucesso!");
 		PresidioDao dao = new PresidioDao();
 		dao.salvar(presidio);
 
@@ -46,50 +44,47 @@ public class PresidioController {
 
 	@RequestMapping("listarPresidio")
 	public String listarPresidio(Presidio presidio, Model model) {
-		
+
 		PresidioDao dao2 = new PresidioDao();
 		PresidioDao dao = new PresidioDao();
 
 		List<Presidio> listaPresidio = dao.listar();
-		
 
 		model.addAttribute("listaPresidio", listaPresidio);
 
 		return "presidio/pesquisarPresidio";
 	}
-	 //=========================================================//
+
+	// =========================================================//
 	@RequestMapping("pesquisarPresidio")
-	public @ResponseBody String pesquisarPresidio(@RequestParam String nomePresidio,@RequestParam String estadoPresidio, HttpServletResponse response, Model model) {
-		
+	public @ResponseBody String pesquisarPresidio(@RequestParam String nomePresidio,
+			@RequestParam String estadoPresidio, HttpServletResponse response) {
+
 		PresidioDao dao2 = new PresidioDao();
-		
+
 		List<Presidio> listaPresidio = dao2.pesquisar(nomePresidio, estadoPresidio);
-		model.addAttribute("listaPresidio", listaPresidio);
-		
+
 		StringBuilder st = new StringBuilder();
 		st.append("<tr style='background-color: #E6E6E6; font-weight: bold;'>");
 		st.append("<td> Nome </td>");
 		st.append("<td> Estado </td>");
-		
+
 		st.append("</tr>");
 		for (Presidio presidio : listaPresidio) {
-		st.append("<tr>");
-		st.append("<td> " + presidio.getNomePresidio() + " </td>");
-		st.append("<td> " + presidio.getEstadoPresidio() + " </td>");
-		
-		
-		st.append("<td>");
-		st.append("<a href='exibirAlterarProduto?id=" + presidio.getIdPresidio() + "'>Editar</a> &nbsp;");
-		st.append("<a href='removerProduto?id=" + presidio.getIdPresidio() + "'>Remover</a>");
-		st.append("</td>");
-		st.append("</tr>");
+			st.append("<tr>");
+			st.append("<td> " + presidio.getNomePresidio() + " </td>");
+			st.append("<td> " + presidio.getEstadoPresidio() + " </td>");
+
+			st.append("<td>");
+			st.append("<a href='exibirAlterarProduto?id=" + presidio.getIdPresidio() + "'>Editar</a> &nbsp;");
+			st.append("<a href='removerProduto?id=" + presidio.getIdPresidio() + "'>Remover</a>");
+			st.append("</td>");
+			st.append("</tr>");
 		}
 		response.setStatus(200);
 		return st.toString();
-		}
-	
-	
-	
+	}
+
 	// ===================================================//
 
 	@RequestMapping("exibirAlterarPresidio")
@@ -98,9 +93,6 @@ public class PresidioController {
 		PresidioDao dao = new PresidioDao();
 		Presidio presidioCompleto = dao.buscarPorId(id);
 		model.addAttribute("presidio", presidioCompleto);
-
-		
-
 
 		return "presidio/AlterarPresidio";
 
@@ -111,8 +103,8 @@ public class PresidioController {
 
 		if (result.hasErrors()) {
 			return "forward:exibirCadastrarPresidio";
-			}
-		
+		}
+
 		PresidioDao dao = new PresidioDao();
 		dao.alterar(Presidio);
 		model.addAttribute("msg", "O presidio " + Presidio.getNomePresidio() + " foi alterado com sucesso !");
@@ -122,13 +114,13 @@ public class PresidioController {
 	}
 
 	@RequestMapping("/removerPresidio")
-    public String removerPresidio(int id, Model model) {
+	public String removerPresidio(int id, Model model) {
 
-	PresidioDao dao = new PresidioDao();
-	dao.remover(id);
-	model.addAttribute("msg", "Presidio Removido com Sucesso !");
+		PresidioDao dao = new PresidioDao();
+		dao.remover(id);
+		model.addAttribute("msg", "Presidio Removido com Sucesso !");
 
-	return "forward:listarPresidio";
-   
+		return "forward:listarPresidio";
+
 	}
 }
